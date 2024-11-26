c996f6e98c6cf1252b9ea946ad0126c61c991b5a
miguelcrs08 <miguelcrs088@gmail.com>
2024-11-26 04:49:57 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Física',
+    'Qual é a diferença entre massa e peso?',
+    'Massa é a quantidade de matéria de um corpo, medida em quilogramas (kg), enquanto peso é a força gravitacional que age sobre essa massa, medida em newtons (N).'
+)
+
+criaCartao(
+    'Química',
+    'O que é uma reação endotérmica?',
+    'É uma reação química que absorve energia do ambiente, geralmente na forma de calor.'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual é a função da clorofila nas plantas?',
+    'Absorver a luz solar para realizar a fotossíntese, produzindo energia para a planta.'
+)
+
+criaCartao(
+    'Matemática',
+    'O que é o MDC (Máximo Divisor Comum)?',
+    'É o maior número que divide dois ou mais números inteiros ao mesmo tempo, sem deixar resto.'
+)
